87d04e5f5835c35855e601a8f71d7e0774ad3364
cdsbl <christy.lindsey@att.net>
2018-04-17 15:58:39 -0500

    Finished exercises

---
+++ ch02/Time.java
@@ -0,0 +1,19 @@
+public class Time
+{
+    public static void main(String[] args)
+    {
+        int hour = 15;
+        int minute = 39;
+        int second = 43;
+        System.out.println(hour);
+        System.out.println(minute);
+        System.out.println(second);
+
+
+System.out.println(midnight);
+
+
+    }
+}
+
+
