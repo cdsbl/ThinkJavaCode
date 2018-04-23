bfcc2d617e6fe1348c31b674db765f5629ca905b
cdsbl <christy.lindsey@att.net>
2018-04-23 16:08:28 -0500

    For

---
+++ ch07/Choice.java
@@ -0,0 +1,12 @@
+public class Choice
+{
+    public static void main(String[] args)
+    {
+        int num = 10;
+        for (int i = 0 + 1; i <= 10; i++)
+        {
+            System.out.println("The number is = " + i);
+        }
+    }
+
+}
